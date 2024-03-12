6a77d8a7960ba852aee1d4ca22181531931bad31
Douglas Franklin <douglas.silva@livelo.com.br>
2024-03-11 23:24:02 -0300

    Integrando Home com CMS

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import S from '../styles/home.module.scss'
 
@@ -6,7 +7,27 @@ import Image from 'next/image'
 
 import techsImage from '../../public/images/techs.svg'
 
-export default function Home() {
+import { getPrismicClient } from '../services/prismic'
+import Prismic from '@prismicio/client'
+import { RichText } from 'prismic-dom'
+
+
+type Content = {
+  title: string
+  subtitle: string
+  linkAction: string
+  titleMobile: string
+  contentMobile: string
+  mobileBanner: string
+  titleWeb: string
+  webContent: string
+  webBanner: string
+}
+interface ContentProps {
+  content: Content
+}
+
+export default function Home({ content }: ContentProps) {
   return (
     <>
       <Head>
@@ -15,9 +36,9 @@ export default function Home() {
       <main className={S.container}>
         <div className={S.containerHeader}>
           <section>
-            <h1>Levando você para o próximo nível!</h1>
-            <span>Uma plataforma com cursos que vão do zero até o profissional na prática, direto ao ponto aplicando o que usamos no mercado de trabalho.</span>
-            <a>
+            <h1>{content.title}</h1>
+            <span>{content.subtitle}</span>
+            <a href={content.linkAction} target='_blank'>
               <button>COMEÇAR AGORA!</button>
             </a>
           </section>
@@ -29,21 +50,21 @@ export default function Home() {
 
         <div className={S.sectionContent}>
           <section>
-            <h2>Aprenda criar aplicativos para Android e iOS</h2>
-            <span>Você vai descobrir o jeito mais moderno de desenvolver apps nativos para iOS e Android, construindo aplicativos do zero até aplicativos</span>
+            <h2>{content.titleMobile}</h2>
+            <span>{content.contentMobile}</span>
           </section>
 
-          <img src="/images/financasApp.png" alt="Conteúdos de desenvolvimento Mobile" />
+          <img src={content.mobileBanner} alt="Conteúdos de desenvolvimento Mobile" />
         </div>
 
         <hr className={S.divisor} />
 
         <div className={S.sectionContent}>
-          <img src="/images/webDev.png" alt="Desenvolvimento de desenvolvimento Mobile" />
+          <img src={content.webBanner} alt="Desenvolvimento de desenvolvimento Mobile" />
 
           <section>
-            <h2>Aprenda criar sistemas web</h2>
-            <span>Criar sistemas, sites usando as tecnologias mais modernas e requisitadas pelo mercado</span>
+            <h2>{content.titleWeb}</h2>
+            <span>{content.webContent}</span>
           </section>
         </div>
 
@@ -58,4 +79,33 @@ export default function Home() {
       </main>
     </>
   );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient()
+
+  const response = await prismic.query([
+    Prismic.Predicates.at('document.type', 'home')
+  ])
+
+  const { title, subtitle, linkaction, titlemobile, contentmobile, mobilebanner, titleweb, webcontent, webbanner } = response.results[0].data
+
+  const content = {
+    title: RichText.asText(title),
+    subtitle: RichText.asText(subtitle),
+    linkAction: linkaction.url,
+    titleMobile: RichText.asText(titlemobile),
+    contentMobile: RichText.asText(contentmobile),
+    mobileBanner: mobilebanner.url,
+    titleWeb: RichText.asText(titleweb),
+    webContent: RichText.asText(webcontent),
+    webBanner: webbanner.url
+  }
+
+  return {
+    props: {
+      content
+    },
+    revalidate: 60 * 2 // regerada a cada 2 minutos
+  }
 }
